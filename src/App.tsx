97e0c8b4d96b3bab445cde1551834e6203b29ea7
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2024-02-22 06:36:49 +0100

    chore: add prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-        <div><div><p>Navbar</p></div></div>
+      <div>
         <div>
-            <div><p>Topbar</p></div>
-            <div><h1>Vite + React</h1>
-                <div className="card">
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p></div>
+          <p>Navbar</p>
         </div>
-
-
-
+      </div>
+      <div>
+        <div>
+          <p>Topbar</p>
+        </div>
+        <div>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
